64ee723c3fbe3f1d18df754904857fc610c18a4b
Katlego Motsepe <ntsakomotsepe7@gmail.com>
2026-05-11 16:05:44 +0200

    11 May 2026
    Basically added everything. Need to add correct info tho.

--- southern-point-suppliers/src/app/pages/about-page/about-page.ts
+++ southern-point-suppliers/src/app/pages/about-page/about-page.ts
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: './about-page.html',
   styleUrl: './about-page.scss',
 })
-export class AboutPage {}
+export class AboutPage {
+
+}
--- southern-point-suppliers/src/app/pages/brands-page/brands-page.ts
+++ southern-point-suppliers/src/app/pages/brands-page/brands-page.ts
@@ -6,4 +6,33 @@ import { Component } from '@angular/core';
   templateUrl: './brands-page.html',
   styleUrl: './brands-page.scss',
 })
-export class BrandsPage {}
+export class BrandsPage {
+
+  brands = [
+    {
+      name: 'Google',
+      logo: 'assets/brands/google.png'
+    },
+    {
+      name: 'Microsoft',
+      logo: 'assets/brands/microsoft.png'
+    },
+    {
+      name: 'Spotify',
+      logo: 'assets/brands/spotify.png'
+    },
+    {
+      name: 'Netflix',
+      logo: 'assets/brands/netflix.png'
+    },
+    {
+      name: 'Apple',
+      logo: 'assets/brands/apple.png'
+    },
+    {
+      name: 'Amazon',
+      logo: 'assets/brands/amazon.png'
+    }
+  ];
+
+}
